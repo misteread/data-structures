76dc89053ebb1956147436e4706c742a66cf5799
chiefportable <kwamea236@protonmail.com>
2025-01-04 03:31:15 +0000

    chng: functions added and a name changed

--- stack/stack.js
+++ stack/stack.js
@@ -1,7 +1,7 @@
 
 /** 
  * 
- *Implementation of a stack
+ *Implementation of a stack data structure with arrays
  * 
  * Described as Last in First Out
  * 
@@ -12,19 +12,48 @@ class Stack{
 		this.#data = data
 	}
 
+	/** 
+	 * Push an element onto the stack
+	 * */
 	push(el){
 		this.#data.push(el)
 	}
 
+	/** 
+	 * Removes an element from the stack
+	 * */
 	pop(){
+		if(this.isEmpty()){
+			return "stack is empty"
+		}
 		return this.#data.pop();
 	}
 
-	read(){
+	/** 
+	 * Read the first element on the stack
+	 * */
+	peek(){
 		return this.#data[this.#data.length -1];
 	}
 
-	all_data(){
-		return this.#data;
+	/** 
+	 * return the size of the stack
+	 * */
+	size(){
+		return this.#data.length;
+	}
+
+	/** 
+	 * Checks if the stack is empty
+	 * */
+	isEmpty(){
+		return this.#data.length === 0;
+	}
+
+	/** 
+	 * Clears all data from the array
+	 * */
+	clear(){
+		return this.#data = [];
 	}
 }
